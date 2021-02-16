869cd2a3a7dddf9acbc4975ec91090100ee8865a
xdvz <imxras@gmail.com>
2021-02-16 20:28:16 +0300

    1/4 fetch api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+};
+
+getResource('https://swapi.dev/api/people/1')
+    .then((body) => {
+        console.log(body);
+    })
+
+//АНАЛОГ ВЫШЕ НО ВЫГЛЯДИТ ЛУЧШЕ
+
+fetch('')
+    .then((res) => {
+        return res.json();
+    })
+.then((body) => {
+    console.log(body);
+})
