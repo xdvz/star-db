28005b22e42c7060eeddb0456f1f3155fc78d991
xdvz <imxras@gmail.com>
2021-02-16 20:37:22 +0300

    1/5 print fetch error

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@
 
 const getResource = async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}` +
+            `, received ${res.status}`)
+    }
     const body = await res.json();
     return body;
 };
 
-getResource('https://swapi.dev/api/people/1')
+getResource('https://swapi.dev/api/people/123123123')
     .then((body) => {
         console.log(body);
-    })
-
-//АНАЛОГ ВЫШЕ НО ВЫГЛЯДИТ ЛУЧШЕ
-
-fetch('')
-    .then((res) => {
-        return res.json();
-    })
-.then((body) => {
-    console.log(body);
+    }).catch((err) => {
+        console.error('Could not fetch', err);  //обработка ошибок
 })
