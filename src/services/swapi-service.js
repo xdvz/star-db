626692d0106e0a863411dfe9754ea06c95cec205
xdvz <imxras@gmail.com>
2021-02-21 01:26:54 +0300

    6/9 structure

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -14,40 +14,78 @@ export default class SwapiService {
 
     async getAllPeople() {
         const res = await this.getResource('/people/');
-        return res.results;
+        return res.results.map(this._transformPerson());
     }
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`)
+    async getPerson(id) {
+        const person = await this.getResource(`/people/${id}/`)
+        return this._transformPerson(person)
     }
 
     async getAllPlanets() {
         const res = await this.getResource('/planets/');
-        return res.results;
+        return res.results.map(this._transformPlanet);
     }
 
-    getPlanet(id) {
-        return this.getResource(`/planets/${id}/`)
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(planet);
     }
 
     async getAllStarships() {
         const res = await this.getResource('/starships/');
-        return res.results;
+        return res.results.map(this._transformStarship);
     }
 
-    getStarships(id) {
-        return this.getResource(`/starships/${id}/`)
+    async getStarships(id) {
+        const starship = await this.getResource(`/starships/${id}/`)
+        return this._transformStarship(starship)
+    }
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+
+        };
+    }
+
+    _transformStarship(starship) {
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.cost_in_credits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargo_capacity
+        }
+    }
+    _transformPerson(person) {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birth_year,
+            eyeColor: person.eye_color
+        }
+
+
     }
 
 
 }
 
-// const swapi = new SwapiService();
-//
-// swapi.getAllPeople().then((people) => {
-//     people.forEach((p) => {
-//         console.log(p.name);
-//     })
-// })
+
 
 
